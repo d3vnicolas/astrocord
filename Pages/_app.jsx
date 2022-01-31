d3064d791a0c05b8f3164913f5092678b131bd0f
Nicolas Soares <devnicolas.coroa@gmail.com>
2022-01-31 01:07:09 -0300

    style scrollbar

--- Pages/_app.jsx
+++ Pages/_app.jsx
@@ -1,11 +1,14 @@
 function GlobalStyle() {
-    return (
-        <style global jsx>{`
+  return (
+    <style global jsx>{`
         * {
           margin: 0;
           padding: 0;
           box-sizing: border-box;
           list-style: none;
+          -webkit-tap-highlight-color: transparent;
+          -moz-tap-highlight-color: transparent;
+          -ms-tap-highlight-color: transparent;
         }
         body, html{
           width: 100%;
@@ -14,6 +17,19 @@ function GlobalStyle() {
         body {
           font-family: 'Open Sans', sans-serif;
         }
+        *::-webkit-scrollbar {
+          width: 8px;
+        }
+        *::-webkit-scrollbar-track {
+          background: #1e1e1e51;        
+          border-radius: 10px;       
+        }
+        
+        *::-webkit-scrollbar-thumb {
+          background-color: #1e1e1e;    
+          border-radius: 10px;       
+        }
+
         /* App fit Height */ 
         html, body, #__next {
           display: flex;
@@ -28,14 +44,14 @@ function GlobalStyle() {
         }
         /* ./App fit Height */ 
       `}</style>
-    );
+  );
 }
 
 export default function MyApp({ Component, pageProps }) {
-    return (
-        <>
-            <GlobalStyle />
-            <Component {...pageProps} />
-        </>
-    )
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
