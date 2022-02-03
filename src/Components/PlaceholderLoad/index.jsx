75bf815f91601a58a13d187333bccf4a1a6528a3
Nicolas Soares <devnicolas.coroa@gmail.com>
2022-02-03 20:11:17 -0300

    fix responsividade do placeholder load

--- src/Components/PlaceholderLoad/index.jsx
+++ src/Components/PlaceholderLoad/index.jsx
@@ -2,100 +2,113 @@ import React from 'react';
 import ContentLoader from 'react-content-loader';
 
 const MyLoader = (props) => (
-    <ul
-        style={{
-            overflow: 'hidden'
-        }}
-    >
+
+    <>
         <li>
             <ContentLoader
                 speed={2}
-                width={476}
+                width={'100%'}
                 height={124}
-                viewBox="0 0 476 124"
+                viewBox="0 0 100% 124"
                 backgroundColor="#4f4f4f"
                 foregroundColor="#8d8d8d"
                 {...props}
             >
-                <rect x="51" y="18" rx="3" ry="3" width="88" height="6" />
-                <rect x="158" y="18" rx="3" ry="3" width="52" height="6" />
-                <rect x="0" y="56" rx="3" ry="3" width="410" height="6" />
-                <rect x="0" y="72" rx="3" ry="3" width="380" height="6" />
-                <rect x="0" y="88" rx="3" ry="3" width="178" height="6" />
-                <circle cx="20" cy="20" r="20" />
+                <rect x="57" y="13" rx="3" ry="3" width="77" height="6" />
+                <rect x="150" y="13" rx="3" ry="3" width="47" height="6" />
+                <rect x="22" y="50" rx="3" ry="3" width="410" height="6" />
+                <rect x="22" y="68" rx="3" ry="3" width="410" height="6" />
+                <rect x="22" y="87" rx="3" ry="3" width="380" height="6" />
+                <circle cx="32" cy="15" r="15" />
             </ContentLoader>
         </li>
 
         <li>
             <ContentLoader
                 speed={2}
-                width={476}
+                width={'100%'}
                 height={124}
-                viewBox="0 0 476 124"
+                viewBox="0 0 100% 124"
                 backgroundColor="#4f4f4f"
                 foregroundColor="#8d8d8d"
                 {...props}
             >
-                <rect x="51" y="18" rx="3" ry="3" width="88" height="6" />
-                <rect x="158" y="18" rx="3" ry="3" width="52" height="6" />
-                <rect x="3" y="62" rx="3" ry="3" width="380" height="6" />
-                <rect x="3" y="79" rx="3" ry="3" width="178" height="6" />
-                <circle cx="20" cy="20" r="20" />
+                <rect x="57" y="13" rx="3" ry="3" width="77" height="6" />
+                <rect x="150" y="13" rx="3" ry="3" width="47" height="6" />
+                <rect x="22" y="50" rx="3" ry="3" width="410" height="6" />
+                <rect x="22" y="67" rx="3" ry="3" width="380" height="6" />
+                <circle cx="32" cy="15" r="15" />
             </ContentLoader>
         </li>
 
         <li>
             <ContentLoader
                 speed={2}
-                width={476}
+                width={'100%'}
                 height={324}
-                viewBox="0 0 476 304"
+                viewBox="0 0 100% 304"
                 backgroundColor="#4f4f4f"
                 foregroundColor="#8d8d8d"
                 {...props}
             >
-                <rect x="51" y="18" rx="3" ry="3" width="88" height="6" />
-                <rect x="158" y="18" rx="3" ry="3" width="52" height="6" />
-                <rect x="3" y="52" rx="3" ry="3" width="312" height="230" />
-                <circle cx="20" cy="20" r="20" />
+                <rect x="22" y="52" rx="3" ry="3" width="265" height="230" />
+                <rect x="57" y="13" rx="3" ry="3" width="77" height="6" />
+                <rect x="150" y="13" rx="3" ry="3" width="47" height="6" />
+                <circle cx="32" cy="15" r="15" />
             </ContentLoader>
         </li>
         <li>
             <ContentLoader
                 speed={2}
-                width={476}
+                width={'100%'}
                 height={124}
-                viewBox="0 0 476 124"
+                viewBox="0 0 100% 104"
                 backgroundColor="#4f4f4f"
                 foregroundColor="#8d8d8d"
                 {...props}
             >
-                <rect x="51" y="18" rx="3" ry="3" width="88" height="6" />
-                <rect x="158" y="18" rx="3" ry="3" width="52" height="6" />
-                <rect x="3" y="62" rx="3" ry="3" width="380" height="6" />
-                <rect x="3" y="79" rx="3" ry="3" width="178" height="6" />
-                <circle cx="20" cy="20" r="20" />
+                <rect x="57" y="13" rx="3" ry="3" width="77" height="6" />
+                <rect x="150" y="13" rx="3" ry="3" width="47" height="6" />
+                <rect x="22" y="50" rx="3" ry="3" width="410" height="6" />
+                <rect x="22" y="67" rx="3" ry="3" width="380" height="6" />
+                <circle cx="32" cy="15" r="15" />
             </ContentLoader>
         </li>
         <li>
             <ContentLoader
                 speed={2}
-                width={476}
+                width={'100%'}
                 height={124}
-                viewBox="0 0 476 124"
+                viewBox="0 0 100% 124"
                 backgroundColor="#4f4f4f"
                 foregroundColor="#8d8d8d"
                 {...props}
             >
-                <rect x="51" y="18" rx="3" ry="3" width="88" height="6" />
-                <rect x="158" y="18" rx="3" ry="3" width="52" height="6" />
-                <rect x="0" y="56" rx="3" ry="3" width="410" height="6" />
-                <rect x="0" y="72" rx="3" ry="3" width="380" height="6" />
-                <rect x="0" y="88" rx="3" ry="3" width="178" height="6" />
-                <circle cx="20" cy="20" r="20" />
+                <rect x="57" y="13" rx="3" ry="3" width="77" height="6" />
+                <rect x="150" y="13" rx="3" ry="3" width="47" height="6" />
+                <rect x="22" y="50" rx="3" ry="3" width="410" height="6" />
+                <rect x="22" y="67" rx="3" ry="3" width="380" height="6" />
+                <rect x="22" y="84" rx="3" ry="3" width="178" height="6" />
+                <circle cx="32" cy="15" r="15" />
             </ContentLoader>
         </li>
-    </ul>
+        <li tabIndex={1}>
+            <ContentLoader
+                speed={2}
+                width={'100%'}
+                height={324}
+                viewBox="0 0 100% 304"
+                backgroundColor="#4f4f4f"
+                foregroundColor="#8d8d8d"
+                {...props}
+            >
+                <rect x="22" y="52" rx="3" ry="3" width="265" height="230" />
+                <rect x="57" y="13" rx="3" ry="3" width="77" height="6" />
+                <rect x="150" y="13" rx="3" ry="3" width="47" height="6" />
+                <circle cx="32" cy="15" r="15" />
+            </ContentLoader>
+        </li>
+    </>
 )
 
-export default MyLoader
+export default MyLoader;
